70db79dae0a0b52a2bfba9d9f313d363f07db537
aalieff <aliefanarkis@gmail.com>
2021-10-04 11:52:49 +0700

    add love button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 var loveBtn = document.querySelector(".btn-love");
+var loveIcon = document.querySelector(".bi-heart");
+// loveIcon.classList.remove('bi-heart');
+// loveIcon.classList.add('bi-heart-fill');
 
-loveBtn.addEventListener("click", function(){
-    document.querySelector(".bi-heart").classList.contains('bi-heart-fill')
+loveBtn.addEventListener("click", () => {
+    // console.log(loveIcon.classList);
+    if( loveIcon.classList.contains = 'bi' ){
+        loveIcon.classList.remove('bi-heart');
+        loveIcon.classList.add('bi-heart-fill');
+    }else{
+        loveIcon.classList.remove('bi-heart-fill');
+        loveIcon.classList.add('bi-heart');
+    }
 })
+
+
